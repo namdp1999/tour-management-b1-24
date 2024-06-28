0b1bc46a3237409531ef80bb1da2d043f19163f2
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-28 20:26:19 +0700

    2.1. Tạo routes

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import sequelize from "./config/database";
-import Tour from "./models/tour.model";
+import clientRoutes from "./routes/client/index.route";
 
 sequelize;
 
@@ -12,23 +12,7 @@ const port: (number | string) = `${process.env.PORT}` || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/tours", async (req: Request, res: Response) => {
-  // SELECT * FROM tours WHERE deleted = false AND status = "active";
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true
-  });
-
-  console.log(tours);
-
-  res.render("client/pages/tours/index", {
-    pageTitle: "Danh sách tour",
-    tours: tours
-  });
-});
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
