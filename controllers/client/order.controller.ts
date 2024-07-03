a7dba88e66b7eae9e69236393283a0f6a376b15f
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-07-03 19:53:05 +0700

    3.1 + 3.2

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -58,3 +58,55 @@ export const order = async (req: Request, res: Response) => {
     orderCode: code
   });
 };
+
+// [GET] /order/success
+export const success = async (req: Request, res: Response) => {
+  const orderCode = req.query.orderCode;
+
+  const order = await Order.findOne({
+    where: {
+      code: orderCode,
+      deleted: false
+    },
+    raw: true
+  });
+
+  const ordersItem = await OrderItem.findAll({
+    where: {
+      orderId: order["id"]
+    },
+    raw: true
+  });
+
+  let totalPrice = 0;
+
+  for (const item of ordersItem) {
+    item["price_special"] = item["price"] * (1 - item["discount"]/100);
+    item["total"] = item["price_special"] * item["quantity"];
+    totalPrice += item["total"];
+
+    const tourInfo = await Tour.findOne({
+      where: {
+        id: item["tourId"],
+        deleted: false,
+        status: "active"
+      },
+      raw: true
+    });
+
+    if(tourInfo["images"]) {
+      tourInfo["images"] = JSON.parse(tourInfo["images"]);
+      item["image"] = tourInfo["images"][0];
+    }
+
+    item["title"] = tourInfo["title"];
+    item["slug"] = tourInfo["slug"];
+  }
+
+  res.render("client/pages/order/success", {
+    pageTitle: "Đặt hàng thành công",
+    order: order,
+    ordersItem: ordersItem,
+    totalPrice: totalPrice
+  });
+};
