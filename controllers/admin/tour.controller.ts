a7017964afb63c58398a0200d7869fc8d6e5006e
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-07-05 19:13:41 +0700

    2.2. Bổ sung upload nhiều ảnh

--- controllers/admin/tour.controller.ts
+++ controllers/admin/tour.controller.ts
@@ -59,6 +59,7 @@ export const createPost = async (req: Request, res: Response) => {
   const dataTour = {
     title: req.body.title,
     code: "",
+    images: JSON.stringify(req.body.images),
     price: parseInt(req.body.price),
     discount: parseInt(req.body.discount),
     timeStart: req.body.timeStart,
